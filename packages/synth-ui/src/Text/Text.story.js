e87224c431d16020a573254e36d1124ff160f0a7
Elving Rodriguez <elving.pr@gmail.com>
2019-08-22 12:36:30 -0400

    chore: Add centered addon.

--- packages/synth-ui/src/Text/Text.story.js
+++ packages/synth-ui/src/Text/Text.story.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import centered from '@storybook/addon-centered/react'
 
 import Text from './Text'
 
-storiesOf('Text', module).add('default', () => (
-  <Text>
-    Lorem ipsum dolor sit amet, duo nullam denique ut, nec at suas illud nostro.
-  </Text>
-))
+storiesOf('Text', module)
+  .addDecorator(centered)
+  .add('default', () => (
+    <Text>
+      Lorem ipsum dolor sit amet, duo nullam denique ut, nec at suas illud
+      nostro.
+    </Text>
+  ))
