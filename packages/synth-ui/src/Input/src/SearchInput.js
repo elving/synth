3e781b0dedc435f57eb21d5b1ac6bf6e6fbd64cf
Elving Rodriguez <elving.pr@gmail.com>
2019-08-27 09:00:13 -0400

    fix: Fix <SearchInput/> padding-left to use new @icons token sizes.

--- packages/synth-ui/src/Input/src/SearchInput.js
+++ packages/synth-ui/src/Input/src/SearchInput.js
@@ -5,43 +5,89 @@ import { withSynth } from '@beatgig/synth-react'
 import { Input } from '../../Input'
 import { SearchIcon } from '../../Icons/src'
 
+/**
+ * @typedef {object} Props
+ * @property {object} synth
+ */
+
 const SearchInputContainer = styled.label`
   align-items: center;
   display: flex;
   position: relative;
 `
 
-const CustomSearchIcon = withSynth(styled(SearchIcon).attrs(({ synth }) => ({
-  size: synth.getUnit('@fontSizes.6'),
-}))`
-  fill: ${({ synth }) => synth.getValue('@Porcelain')};
-  left: ${({ synth }) => synth.getUnit('@spacing.1')};
+/**
+ * @param {Props} props
+ * @returns {string}
+ */
+const setIconFill = ({ synth }) => synth.getValue('@Porcelain')
+
+/**
+ * @param {Props} props
+ * @returns {string}
+ */
+const setIconLeft = ({ synth }) => synth.getUnit('@spacing.1')
+
+/**
+ * @param {Props} props
+ * @returns {string}
+ */
+const setIconFillFocus = ({ synth }) => synth.getValue('@BalticSea')
+
+const CustomSearchIcon = withSynth(styled(SearchIcon)`
+  fill: ${setIconFill};
+  left: ${setIconLeft};
   pointer-events: none;
   position: absolute;
   z-index: 5;
 `)
+
+/**
+ * @param {Props} props
+ * @returns {string}
+ */
+const setInputPaddingLeft = ({ synth }) => `
+  calc(${synth.getUnit('@icons')} + ${synth.getUnit(
+  '@spacing.1',
+)} + ${synth.getUnit('size:border:control')})
+`
+
+/**
+ * @param {Props} props
+ * @returns {string}
+ */
+const setInputBackgroundColorFocus = ({ synth }) => synth.getValue('@Porcelain')
+
+/**
+ * @param {Props} props
+ * @returns {string}
+ */
+const setInputBorderColorFocus = ({ synth }) => synth.getValue('@Porcelain')
+
+/**
+ * @param {Props} props
+ * @returns {string}
+ */
+const setInputColorFocus = ({ synth }) => synth.getValue('@BalticSea')
 
 const CustomInput = withSynth(styled(Input).attrs(() => ({
   type: 'search',
 }))`
   transition: all 0.2s ease-in-out 0s;
   position: relative;
-  padding-left: calc(
-    ${({ synth }) => synth.getUnit('@fontSizes.6')} +
-      ${({ synth }) => synth.getUnit('@spacing.2')}
-  );
+  padding-left: ${setInputPaddingLeft};
   z-index: 1;
   width: 200px;
 
   :focus {
-    background-color: ${({ synth }) => synth.getValue('@Porcelain')};
-    border-color: ${({ synth }) => synth.getValue('@Porcelain')};
-    color: ${({ synth }) => synth.getValue('@BalticSea')};
+    background-color: ${setInputBackgroundColorFocus};
+    border-color: ${setInputBorderColorFocus};
+    color: ${setInputColorFocus};
     outline: 0 none;
     width: 320px;
 
     & + svg {
-      fill: ${({ synth }) => synth.getValue('@BalticSea')};
+      fill: ${setIconFillFocus};
     }
   }
 `)
