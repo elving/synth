0f657fb07fdb1096923ba74b5abeb278cf47f882
Elving Rodriguez <elving.pr@gmail.com>
2019-12-03 18:23:53 -0400

    fix(types): pass along className prop to <Icon> component

--- packages/synth-ui/src/Icons/ChevronDownIcon.js
+++ packages/synth-ui/src/Icons/ChevronDownIcon.js
@@ -6,7 +6,7 @@ import { Icon } from '../Icon'
 import { withForwardedRef } from '../utils'
 
 const ChevronDownIcon = ({ className = '', ref, ...props }) => (
-  <Icon {...props} ref={ref} viewBox="0 0 24 24">
+  <Icon {...props} className={className} ref={ref} viewBox="0 0 24 24">
     <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
     <path d="M0 0h24v24H0z" fill="none" />
   </Icon>
--- packages/synth-ui/src/Icons/CloseIcon.js
+++ packages/synth-ui/src/Icons/CloseIcon.js
@@ -6,7 +6,7 @@ import { Icon } from '../Icon'
 import { withForwardedRef } from '../utils'
 
 const CloseIcon = ({ className = '', ref, ...props }) => (
-  <Icon {...props} ref={ref} viewBox="0 0 24 24">
+  <Icon {...props} className={className} ref={ref} viewBox="0 0 24 24">
     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
     <path d="M0 0h24v24H0z" fill="none" />
   </Icon>
