ee2ef68a6615469d8c90fbd46f098638f250431e
Elving Rodriguez <elving.pr@gmail.com>
2019-12-07 22:52:16 -0400

    fix(types): add xmlns to svgs and add currentColor as default fill

--- packages/synth-ui/src/Icons/AdminIcon.js
+++ packages/synth-ui/src/Icons/AdminIcon.js
@@ -7,7 +7,7 @@ const AdminIcon = forwardRef(
    * @param {import('@beatgig/synth-ui').IconProps & import('@beatgig/synth-react').SynthComponentProps} props
    */
   (props, ref) => (
-    <Icon {...props} fill="currentColor" ref={ref} viewBox="0 0 24 24">
+    <Icon {...props} ref={ref} viewBox="0 0 24 24">
       <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z" />
       <path d="M0 0h24v24H0z" fill="none" />
     </Icon>
--- packages/synth-ui/src/Icons/CheckIcon.js
+++ packages/synth-ui/src/Icons/CheckIcon.js
@@ -7,7 +7,7 @@ const CheckIcon = forwardRef(
    * @param {import('@beatgig/synth-ui').IconProps & import('@beatgig/synth-react').SynthComponentProps} props
    */
   (props, ref) => (
-    <Icon {...props} fill="currentColor" ref={ref} viewBox="0 0 24 24">
+    <Icon {...props} ref={ref} viewBox="0 0 24 24">
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
     </Icon>
