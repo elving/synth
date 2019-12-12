7dd56d28cadf97d0d3abac888120b2f6551de43d
Elving Rodriguez <elving.pr@gmail.com>
2019-12-12 17:02:04 -0400

    chore(release): docs

--- docs/main.0ec62deee822c5abbbdd.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(388),c(509),c(1139),n.exports=c(1141)},1:function(n,o){},509:function(n,o,c){"use strict";c.r(o);c(510),c(1136)}},[[0,1,2]]]);
