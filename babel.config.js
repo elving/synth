19b8f68f95c4b1e9785cf0fa4e0cd655499cbc81
Elving Rodriguez <elving.pr@gmail.com>
2020-01-27 00:03:43 -0400

    chore: update dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@ module.exports = {
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
     'babel-plugin-add-react-displayname',
     [
       'babel-plugin-styled-components',
