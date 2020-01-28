cd271e3647710370381896dc9665e515f92fef2c
Elving Rodriguez <elving.pr@gmail.com>
2020-01-27 22:21:54 -0400

    test: fix CSS bindings test

--- packages/synth-css/tests/bindings.spec.js
+++ packages/synth-css/tests/bindings.spec.js
@@ -165,6 +165,10 @@ const tokens = {
       base: 'normal',
       emphasis: 'italic',
     },
+    opacity: {
+      base: 1,
+      disabled: 0.35,
+    },
   },
 
   typography: {
@@ -203,6 +207,7 @@ describe('css bindings', () => {
     const [category, property] = tokenPrefix.split(':')
     const binding = toCamelCase(cssProperty)
 
+    console.log(category, property)
     Object.keys(tokens[category][property]).forEach((tokenName) => {
       test(`${binding}(${tokenName})`, () => {
         const bindingValue = bindings[binding](tokens, tokenName)
